4d5ff249d1bccae288243ecbb7c5d37dbb220229
telleQ <STEP.nst@gmail.com>
2020-10-13 22:51:22 +0300

    init: worked

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    let str = Math.abs(n).toString();
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
 }
